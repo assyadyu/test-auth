978099fa16ebbcf6240c8ac99a4826fba63dc8c9
assyadyu <assya.dyu@gmail.com>
2025-01-20 12:54:31 +0500

    configs & settings

--- src/app/common/dependencies.py
+++ src/app/common/dependencies.py
@@ -1,12 +1,22 @@
-from fastapi import (
-    FastAPI,
-)
+from fastapi import FastAPI
 
 from app.db.sessions import (
     async_session,
     session_factory,
 )
-
+from app.interfaces.repositories.base import IBaseRepository
+from app.interfaces.repositories.users import IUserRepository
+from app.repositories.sqlalchemy.base import SQLAlchemyBaseRepository
+from app.repositories.sqlalchemy.users import UserRepository
+from app.users.services import (
+    UserService,
+    IUserService,
+)
 
 def bind_dependencies(app: FastAPI, db_url: str):
     app.dependency_overrides[async_session] = session_factory(db_url)
+
+    app.dependency_overrides[IBaseRepository] = SQLAlchemyBaseRepository
+    app.dependency_overrides[IUserRepository] = UserRepository
+
+    app.dependency_overrides[IUserService] = UserService
--- src/app/common/routers.py
+++ src/app/common/routers.py
@@ -3,7 +3,10 @@ from fastapi import (
     FastAPI,
 )
 
+from app.users.routers import user_router
+
 
 def bind_routers(app: FastAPI):
     router = APIRouter(prefix="/api")
+    router.include_router(user_router)
     app.include_router(router=router)
